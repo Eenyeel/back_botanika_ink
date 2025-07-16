a9f0d4750354bfa5517cfa161886c7e8688e3d84
Angel <eenyeel17@gmail.com>
2025-07-16 16:06:50 -0600

    Creacion de controllers de las clases con su respectiva: alta, modificacion ,eliminar de todas las clases menos de mensaje y estado cita ya que esas ya han sido establecidas y sus valores seran los mismos y no habra alguna modificacion. Tambien todos con sus respectivos getAll para mostrar en tablas

--- src/java/botanika_controller/ControllerRol.java
+++ src/java/botanika_controller/ControllerRol.java
@@ -1,10 +1,41 @@
 
 package botanika_controller;
 
+import botanika_core_db.ConectionMySQL;
+import botanika_models.Rol;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Angel Santamaria
  */
 public class ControllerRol {
-    
+        public List<Rol> getAll() throws Exception {
+        String sql = "SELECT * FROM mensaje";
+        List<Rol> rols = new ArrayList<>();
+
+        try (
+            Connection conn = new ConectionMySQL().open();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery()
+        ) {
+            while (rs.next()) {
+                rols.add(fill(rs));
+            }
+        }
+
+        return rols;
+    }
+
+    private Rol fill(ResultSet rs) throws SQLException {
+        Rol r = new Rol();
+        r.setId(rs.getInt("id"));
+        r.setNombre(rs.getString("nombre"));
+        return r;
+    }
 }
